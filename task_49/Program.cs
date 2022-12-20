427628d207cdc46a4c29e1588995a8544e277ee2
VadimTalimonchik <tvadmins@gmail.com>
2022-12-20 11:09:50 +0300

    Задача 49

--- task_49/Program.cs
+++ task_49/Program.cs
@@ -10,4 +10,35 @@
 // 5 81 2 9
 // 8 4 2 4
 
-Console.WriteLine("Hello, World!");
+int[,] CreateNormalMatrix(int a, int b)
+{
+int[,] matrix = new int[a, b];
+for (int rows = 0; rows < matrix.GetLength(0); rows++)
+{
+    for (int columns = 0; columns < matrix.GetLength(1); columns++)
+    {
+        matrix[rows, columns] = new Random().Next(1,10);
+        Console.Write($"{matrix[rows, columns]} ");
+    }
+    Console.WriteLine("");
+}
+Console.WriteLine("");
+return matrix;
+}
+
+void CreateSqrtMatrix(int[,] array)
+{
+int[,] newArray = new int[array.GetLength(0), array.GetLength(1)];
+for (int rows = 0; rows < array.GetLength(0); rows++)
+{
+    for (int columns = 0; columns < array.GetLength(1); columns++)
+    {
+        if(rows%2==0 && columns%2==0) newArray[rows, columns] = (int)Math.Pow(array[rows, columns], 2);
+        else newArray[rows, columns] = array[rows, columns];
+        Console.Write($"{newArray[rows, columns]} ");
+    }
+    Console.WriteLine("");
+}
+}
+CreateSqrtMatrix(CreateNormalMatrix(4,4));
+
